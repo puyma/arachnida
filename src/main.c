209a6fc54dd7572c942c18204008b19c265e5126
puyma <mpuig-ma@student.42barcelona.com>
2023-04-27 23:35:29 +0200

    pipe test did not work out, will have to deal with memory

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 18:02:38 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/04/27 19:51:02 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/04/27 23:33:51 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,11 @@
 #include <fcntl.h>
 #include <curl/curl.h>
 
-static int	ft_crawl(char *url, int depth_level);
-static int	ft_curl_fd(char *url, int fd);
-static int	ft_extract_urls(int fd);
+static int		ft_crawl(char *url, int depth_level);
+static int		ft_curl_fd(char *url, int fd);
+static int		ft_extract_urls(int fd);
+static size_t	write_callback(char *ptr, size_t size, 
+					size_t nmemb, void *userdata);
 
 int	main(int argc, char **argv)
 {
@@ -34,31 +36,20 @@ int	main(int argc, char **argv)
 
 static int	ft_crawl(char *url, int depth_level)
 {
-	int	fd[2];
-	int	err;
-	int	pid;
-
-	err = pipe(fd);
-	if (err == -1)
-		return (2);
-	pid = fork();
-	if (pid == -1)
-		return (3);
-	if (pid == 0)
-	{
-		printf("crawling %s\n", url);
-		close(fd[0]);
-		ft_curl_fd(url, fd[1]);
-	}
-	else
-	{
-		close(fd[1]);
-		wait(NULL);
-		ft_extract_urls(fd[0]);
-		close(fd[0]);
-	}
+	printf("crawling %s\n", url);
+	ft_curl_fd(url, 0);
 	return (0);
 	(void) depth_level;
+	(void) ft_extract_urls;
+}
+
+static size_t write_callback(char *ptr, size_t size, 
+		size_t nmemb, void *userdata)
+{
+	write(1, "\n\noo\n\n", 6);
+	write(1, ptr, nmemb);
+	(void) size; (void) nmemb; (void) userdata;
+	return (0);
 }
 
 static int	ft_extract_urls(int fd)
@@ -80,10 +71,23 @@ static int	ft_curl_fd(char *url, int fd)
 	if (curl)
 	{
 		curl_easy_setopt(curl, CURLOPT_URL, url);
-		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+		curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/7.64.1");
+		curl_easy_setopt(curl, CURLOPT_USE_SSL, (long)CURLUSESSL_ALL);
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 1L);
 		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, fdopen(fd, "w"));
+		curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L);
+		curl_easy_setopt(curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+		curl_easy_setopt(curl, CURLOPT_SSL_ENABLE_ALPN, 1L);
+		curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, (long)CURL_HTTP_VERSION_1_1);
+		//curl_easy_setopt(curl, CURLOPT_WRITEDATA, fdopen(fd, "w"));
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, (void *)NULL);
+		(void) write_callback;
+		curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
+		curl_easy_setopt(curl, CURLOPT_EXPECT_100_TIMEOUT_MS, 3000L);
+		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 20L);
+		curl_easy_setopt(curl, CURLOPT_SERVER_RESPONSE_TIMEOUT, 22L);
+		curl_easy_setopt(curl, CURLOPT_COOKIEJAR, "/tmp/cookies.txt");
 		printf("writing to fd %d\n", fd);
 		res = curl_easy_perform(curl);
 		if (res != CURLE_OK)
