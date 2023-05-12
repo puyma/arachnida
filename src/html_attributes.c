b5bb41a51dc224eb04023e098d4ee7292ee2f067
puyma <mpuig-ma@student.42barcelona.com>
2023-05-12 11:49:28 +0200

    freed strjoin

--- src/html_attributes.c
+++ src/html_attributes.c
@@ -6,7 +6,7 @@
 /*   By: mpuig-ma <mpuig-ma@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 12:10:25 by mpuig-ma          #+#    #+#             */
-/*   Updated: 2023/05/12 11:45:22 by mpuig-ma         ###   ########.fr       */
+/*   Updated: 2023/05/12 11:49:09 by mpuig-ma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,25 @@
 char *
 html_get_attribute (const char *attr, char *html_el)
 {
-	char	*attribute;
 	char	*el;
+	char	*attribute;
+	size_t	attribute_len;
 
-	attribute = ft_strjoin(attr, "=");
 	el = html_el;
+	attribute = ft_strjoin(attr, "=");
+	attribute_len = strlen(attribute);
 	while (*el != '\0')
 	{
-		if (*el == *attr && strncmp (el, attribute, strlen (attribute)) == 0)
+		if (*el == *attr && strncmp (el, attribute, attribute_len) == 0)
 		{
-			el += strlen(attribute);
+			el += attribute_len;
 			if (*el == 34 || *el == 39)
-				return (el);
+			{ free (attribute); return (el); }
 			else
-				return (NULL);
+			{ free (attribute); return (NULL); }
 		}
 		++el;
 	}
+	free (attribute);
 	return (NULL);
 }
